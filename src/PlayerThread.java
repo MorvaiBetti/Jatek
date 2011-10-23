9267b892cc7da05c7e0378634d961e407fd5c84a
MorvaiBetti <morvaibetti@gmail.com>
2011-10-23 13:59:18 +0300

    Angolosítottam

--- src/PlayerThread.java
+++ src/PlayerThread.java
@@ -1,78 +1,77 @@
 import java.lang.management.ManagementFactory;
 
 public class PlayerThread extends Thread implements Player{
-	public String jatekosNeve;
-	public int sorrend;
+	public String playerName;
+	public int sequence;
 	public Player p;
-	public long maxIdo;
-	public long elteltIdo=0;
-	public int kovMunka;
-	public Move elozoLepes=null;
-	public Move kLepes;
+	public long maxTime;
+	public long elapsedTime=0;
+	public Move prevStep=null;
+	public Move nextStep;
 	public QuixoBoard t;
-	public static final int passziv=1;
-	public static final int aktiv=0;
-	public static final int kilepes=2;
-	public int allapot=passziv;
+	public static final int passive=1;
+	public static final int active=0;
+	public static final int exit=2;
+	public int status=passive;
 	
-	public PlayerThread(int sor, long mI, String n) throws InstantiationException, IllegalAccessException, ClassNotFoundException{
+	public PlayerThread(int seq, long mT, String n) throws InstantiationException, IllegalAccessException, ClassNotFoundException{
 		super();
-		this.sorrend=sor;
-		this.maxIdo=mI;
-		this.jatekosNeve=n;
-		p= (Player) Class.forName(jatekosNeve).newInstance();
+		this.sequence=seq;
+		this.maxTime=mT;
+		this.playerName=n;
+		p= (Player) Class.forName(playerName).newInstance();
 	}
 	
 	//Az adott szal adott futasa mennyi idot vett igenybe
-	public long getElteltIdo(){
-	  return ManagementFactory.getThreadMXBean().getThreadUserTime(getId()) / 1000000;
+	public long getElapsedTime(){
+		return ManagementFactory.getThreadMXBean().getThreadUserTime(getId()) / 1000000;
 	}
 	
 	public void run(){
 		try {
-			while(true){
-				switch(allapot){
-					case aktiv: {
-						break;
-					}
-					case kilepes: {
-						return;
-					}
-					default: {
-						sleep(1);
-					}
+		while(true){
+			switch(status){
+				case active: {
+					break;
+				}
+				case exit: {
+					return;
+				}
+				default: {
+					sleep(1);
 				}
 			}
+		}
 		} catch (Exception e) {
-			e.printStackTrace();
+		e.printStackTrace();
 		}
 	}
 	
-	public void adatok(int sorrend, long ido) {
-		allapot=aktiv;
-		p.adatok(sorrend, ido);
-		allapot=passziv;
-	}
-
-	public Move kovLepes(Move eL, long ido) {
-		allapot=aktiv;
-		System.out.println(getName() + ", allapot: " + allapot);
-		kLepes=p.kovLepes(elozoLepes, elteltIdo);
-		allapot=passziv;
-		return kLepes;
-	}
-
-	public int getSzin() {
-		return sorrend;
+	public void datas(int sequence, long time) {
+		status=active;
+		p.datas(sequence, time);
+		status=passive;
 	}
 	
-	public void beallitTablat(QuixoBoard t) {
-		allapot=aktiv;
-		p.beallitTablat(t);
-		allapot=passziv;
+	public Move nextMove(Move pS, long oTime) {
+		status=active;
+		System.out.println(getName() + ", status: " + status);
+		nextStep=p.nextMove(pS, oTime);
+		status=passive;
+		return nextStep;
 	}
 	
-	public void kilep(){
-		allapot=kilepes;
+	public int getColor() {
+		return sequence;
+	}
+	
+	public void setTable(QuixoBoard t) {
+		status=active;
+		p.setTable(t);
+		status=passive;
+	}
+	
+	public void exit(){
+		status=exit;
 	}
 }
