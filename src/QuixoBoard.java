71d4d19872b8c423cdc40e12017400c7492eff17
MorvaiBetti <morvaibetti@gmail.com>
2011-10-23 14:01:57 +0300

    Angolosítottam

--- src/QuixoBoard.java
+++ src/QuixoBoard.java
@@ -4,53 +4,48 @@ public class QuixoBoard implements Cloneable {
 	public static final int empty =0;
 	public static final int X=1;
 	public static final int O=2;
-	public int[][] tabla;
+	public int[][] table;
 	
 	//konstruktor
 	public QuixoBoard(){
-		tabla=new int[5][5];
+		table=new int[5][5];
 		for(int i=0; i<5; i++){
 			for(int j=0; j<5; j++){
-				tabla[i][j]=0;
+				table[i][j]=0;
 			}
 		}
 	}
 	
-	//Tabla masolasa
+	//table clone
 	public Object clone(){
 		QuixoBoard board=new QuixoBoard();
 		for(int i=0; i<5; i++){
 			for(int j=0; j<5; j++){
-				board.tabla[i][j]=this.tabla[i][j];
+				board.table[i][j]=this.table[i][j];
 			}
 		}
 		return board;
 	}
 	
-	//a tabla egy adott mezojenek erteket adja vissza
-	public int getMezo(int x, int y){
-		return tabla[x][y];
+	//a table egy adott mezojenek erteket adja vissza
+	public int getField(int x, int y){
+		return table[x][y];
 	}
 	
 	//Az adott lepes helyes-e?
-	public boolean szabalyos(int x, int y, int minta, int xn, int yn){
-	//	System.out.println("Szabalyos?? "+minta);
+	public boolean legal(int x, int y, int model, int nx, int ny){
 		//csak sajat szinut vagy semlegeset lehet 
-		if(tabla[x][y]!=minta && tabla[x][y]!=0){
+		if(table[x][y]!=model && table[x][y]!=0){
 			return false;
 		}
 		
 		//ahonnan elvettem, oda nem rakhatom vissza
-		if(x==xn && y==yn){
+		if(x==nx && y==ny){
 			return false;
 		}
-	/*	//nem letezo minta
-		if(minta!=1 && minta!=2){
-			return false;
-		}*/
 		
 		//ha nem letezo mezorol akarunk kivenni/visszatenni
-		if(x<0 || y<0 || x>4 || y>4 || xn<0 || yn<0 || xn>4 || yn>4){
+		if(x<0 || y<0 || x>4 || y>4 || nx<0 || ny<0 || nx>4 || ny>4){
 			return false;
 		}
 		
@@ -60,30 +55,30 @@ public class QuixoBoard implements Cloneable {
 		}
 		
 		//csak a palya szelere lehet visszatenni
-		if(xn!=0 && xn!=4 && yn!=0 && yn!=4){
+		if(nx!=0 && nx!=4 && ny!=0 && ny!=4){
 			return false;
 		}
 
 		//melyik kivetel eseten hova lehet tenni
-		if(x==0 && ((xn==x && yn==0) ||(xn==x && yn==4) || (xn==4 && y==yn) )){
+		if(x==0 && ((nx==x && ny==0) ||(nx==x && ny==4) || (nx==4 && y==ny) )){
 			return true;
-		}else if(x==4 &&((x==xn && yn==0) || (xn==x && yn==4) || (xn==0 && yn==y))){
+		}else if(x==4 &&((x==nx && ny==0) || (nx==x && ny==4) || (nx==0 && ny==y))){
 			return true;
-		}else if(y==0 && ((yn==y && xn==0) ||(yn==y && xn==4) || (yn==4 && x==xn) )){
+		}else if(y==0 && ((ny==y && nx==0) ||(ny==y && nx==4) || (ny==4 && x==nx) )){
 			return true;
-		}else if(y==4 &&((y==yn && xn==0) || (yn==y && xn==4) || (yn==0 && xn==x))){
+		}else if(y==4 &&((y==ny && nx==0) || (ny==y && nx==4) || (ny==0 && nx==x))){
 			return true;
 		}
 		return false;
 	}
 	
 	//nyertes-e
-	public boolean nyert(int minta){
+	public boolean win(int model){
 		int db=0;
 		//sorokon megy vegig
 		for(int i=0; i<5; i++){
 			for(int j=0; j<5; j++){
-				if(getMezo(i,j)==minta){
+				if(getField(i,j)==model){
 					db++;
 					if(db==5){
 						return true;
@@ -97,7 +92,7 @@ public class QuixoBoard implements Cloneable {
 		//oszlopokon megy vegig
 		for(int i=0; i<5; i++){
 			for(int j=0; j<5; j++){
-				if(getMezo(j,i)==minta){
+				if(getField(j,i)==model){
 					db++;
 					if(db==5){
 						return true;
@@ -111,13 +106,13 @@ public class QuixoBoard implements Cloneable {
 		//foatlo es mellekatlo
 		int db2=0;
 		for(int i=0; i<5; i++){
-			if(getMezo(i,i)==minta){
+			if(getField(i,i)==model){
 				db++;
 				if(db==5){
 					return true;
 				}
 			}
-			if(getMezo(i, 4-i)==minta){
+			if(getField(i, 4-i)==model){
 				db2++;
 				if(db2==5){
 					return true;
@@ -127,57 +122,53 @@ public class QuixoBoard implements Cloneable {
 		return false;
 	}
 	
-	public void lepestVegrehajt(int x, int y, int minta, int xn, int yn){
-		/*if(!szabalyos(x,y,minta,xn,yn)){
-			System.out.println("TÖKI "+minta);
-			return;
-		}*/
+	public void makeStep(int x, int y, int model, int nx, int ny){
 		//ha az eredeti sorba rakom vissza
-		if(x==xn){
+		if(x==nx){
 			//jobbrol tolok
-			if(y<yn){
+			if(y<ny){
 				for(int i=0; i+y<4; i++){
-					tabla[x][y+i]=tabla[x][y+i+1];
+					table[x][y+i]=table[x][y+i+1];
 				}
-				tabla[xn][yn]=minta;
+				table[nx][ny]=model;
 				return;
 			}
 			//balrol tolok
-			if(y>yn){
+			if(y>ny){
 				for(int i=0; y-i>0; i++){
-					tabla[x][y-i]=tabla[x][y-i-1];
+					table[x][y-i]=table[x][y-i-1];
 				}
-				tabla[xn][yn]=minta;
+				table[nx][ny]=model;
 				return;
 			}
 		}
 		
 		//ha az eredeti oszlopba rakom vissza
-		if(y==yn){
+		if(y==ny){
 			//alulrol tolok
-			if(x<xn){
+			if(x<nx){
 				for(int i=0; i+x<4; i++){
-					tabla[x+i][y]=tabla[x+i+1][y];
+					table[x+i][y]=table[x+i+1][y];
 				}
-				tabla[xn][yn]=minta;
+				table[nx][ny]=model;
 				return;
 			}
 			//felulrol tolok
-			if(x>xn){
+			if(x>nx){
 				for(int i=0; x-i>0; i++){
-					tabla[x-i][y]=tabla[x-i-1][y];
+					table[x-i][y]=table[x-i-1][y];
 				}
-				tabla[xn][yn]=minta;
+				table[nx][ny]=model;
 				return;
 			}
 		}
 	}
 	
 	//adott jatekosnak van-e meg szabalyos lepese
-	public boolean vanSzabalyosLepes(int minta){
+	public boolean existLegalStep(int model){
 		for(int i=0; i<5; i++){
 			for(int j=0; j<5; j++){
-				if(getMezo(i,j)==minta ||getMezo(i,j)==empty){
+				if(getField(i,j)==model ||getField(i,j)==empty){
 					return true;
 				}
 			}
@@ -185,18 +176,17 @@ public class QuixoBoard implements Cloneable {
 		return false;
 	}
 	
-	//tabla kiiratasa
-	public void kiir(){
+	public String toString(){
 		String s="     0  1  2  3  4\n\n 0  ";
 		for(int i=0; i<5; i++){
 			for(int k=0; k<5; k++){
-				if(getMezo(i,k)==empty){
+				if(getField(i,k)==empty){
 					s=s+" - ";
 				}
-				if(getMezo(i,k)==X){
+				if(getField(i,k)==X){
 					s=s+" X ";
 				}
-				if(getMezo(i,k)==O){
+				if(getField(i,k)==O){
 					s=s+" O ";
 				}
 			}
@@ -205,71 +195,6 @@ public class QuixoBoard implements Cloneable {
 			}
 			s=s+"\n " + (i+1)+"  ";
 		}
-		System.out.print(s+"\n ");
+		return s;
 	}
-	
-	//Tesztelesre
-	/*public static void main(String[] args){
-		QuixoBoard t=new QuixoBoard();
-		t.print();
-		System.out.println();
-		t.makeMove(0,0,2,0,2);
-		t.print();
-		System.out.println();
-		t.makeMove(0,0,1,0,4);
-		t.print();
-	}*/
-	/*public static void main(String[] args){
-		QuixoBoard t=new QuixoBoard();
-		t.print();
-		BufferedReader reader = new BufferedReader(new InputStreamReader (System.in));
-		boolean megy=true;
-		int[] m;
-		m=new int[4];
-		int i;
-		String be;
-		StringTokenizer st;
-		while(megy){
-			if(!t.legalMoveExist(1)){
-				megy=false; 
-				break;
-			}
-			i=0;
-			System.out.println("X lép: ");
-			try {
-				be = reader.readLine();
-				st = new StringTokenizer(be);
-			    while (st.hasMoreTokens()) {
-			         m[i]=Integer.parseInt(st.nextToken());
-			         i++;
-			    }
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			    t.makeMove(m[0], m[1], 1, m[2], m[3]);
-			    t.print();
-			    
-		    //masodik jatekos
-			if(!legalMoveExist(2)){
-				megy=false; 
-				break;
-			}
-		    i=0;
-			System.out.println("O lép: ");
-			try {
-				be = reader.readLine();
-				st = new StringTokenizer(be);
-			    while (st.hasMoreTokens()) {
-			         m[i]=Integer.parseInt(st.nextToken());
-			         i++;
-			    }
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		    t.makeMove(m[0], m[1], 2, m[2], m[3]);
-		    t.print();
-		}
-	}*/
 }
