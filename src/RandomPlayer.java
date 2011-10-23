f1075b1033c12dd2d7b13e100ae0b8df832afddd
MorvaiBetti <morvaibetti@gmail.com>
2011-10-23 14:03:47 +0300

    Kezeli a saját tábláját és angolosítottam.

--- src/RandomPlayer.java
+++ src/RandomPlayer.java
@@ -1,65 +1,62 @@
 import java.util.*;
 
 public class RandomPlayer implements Player{
-	public QuixoBoard tabla;
-	public ArrayList<Move> lepesek; 			//lehetseges lepesek letarolasara
-	public int szinem; 							//A jatekos sajat mintaja
-	public int eSzin;							//Az ellenfel mintaja
-	public long osszIdo;
+	public QuixoBoard table;
+	public ArrayList<Move> steps; //lehetseges steps letarolasara
+	public int color; //A jatekos sajat mintaja
+	public int opponentColor; //Az ellenfel mintaja
+	public long maxTime;
 	
-	public void beallitTablat(QuixoBoard qt) {
-		tabla=(QuixoBoard)qt.clone();
+	public void setTable(QuixoBoard qt) {
+		table=qt;
 	}
-
-	public void adatok(int sorrend, long ido) {
-		lepesek=new ArrayList<Move>(56); 		//16*3-4 lehetseges lepes lehet maximum (3*5*4)
-		osszIdo=ido;
-		if(sorrend==1){ 						//A jatekosnak melyik a mintaja, es az ellenfele melyik
-			szinem=QuixoBoard.X;
-			eSzin=QuixoBoard.O;
-		}else if(sorrend==2){szinem=QuixoBoard.O;
-				eSzin=QuixoBoard.X;
+	
+	public void datas(int sequence, long time) {
+		steps=new ArrayList<Move>(56); //16*3-4 lehetseges lepes lehet maximum (3*5*4)
+		maxTime=time;
+		if(sequence==QuixoBoard.X){ //A jatekosnak melyik a mintaja, es az ellenfele melyik
+			color=QuixoBoard.X;
+			opponentColor=QuixoBoard.O;
+		}else if(sequence==QuixoBoard.O){color=QuixoBoard.O;
+		opponentColor=QuixoBoard.X;
 		}
 	}
+	
+	//Az AI kovetkezo lepese. A legal stepset osszegyujti, osszekeveri oket, es valaszt kozuluk egyet, amit meglep
+	public Move nextMove(Move prevStep, long time) {
 
-	//Az AI kovetkezo lepese. A szabalyos lepeseket osszegyujti, osszekeveri oket, es valaszt kozuluk egyet, amit meglep
-	public Move kovLepes(Move elozoLepes, long ido) {
-	  /*
-		lepesek.clear(); 		//torli az eddig lementett lepeseket
-		
-		for(int i=0; i<5; i++){ 		//A lehetseges lepeseken vegigmegy, es lepesek-hez hozzaadja a szabalyos lepeseket
+		table.toString();
+		steps.clear(); //torli az eddig lementett stepset
+		for(int i=0; i<5; i++){ //A lehetseges stepsen vegigmegy, es steps-hez hozzaadja a legal stepset
 			//ha az elso sorbol valasztok
-			if(tabla.szabalyos(0, i, szinem, 0, 4)){lepesek.add(new Move(0, i, 0, 4));}
-			if(tabla.szabalyos(0, i, szinem, 0, 0)){lepesek.add(new Move(0, i, 0, 0));}
-			if(tabla.szabalyos(0, i, szinem, 4, i)){lepesek.add(new Move(0, i, 4, i));}
+			if(table.legal(0, i, color, 0, 4)){steps.add(new Move(0, i, 0, 4));}
+			if(table.legal(0, i, color, 0, 0)){steps.add(new Move(0, i, 0, 0));}
+			if(table.legal(0, i, color, 4, i)){steps.add(new Move(0, i, 4, i));}
 			//ha az utolso sorbol valasztok
-			if(tabla.szabalyos(4, i, szinem, 4, 0)){lepesek.add(new Move(4, i, 4, 0));}
-			if(tabla.szabalyos(4, i, szinem, 4, 4)){lepesek.add(new Move(4, i, 4, 4));}
-			if(tabla.szabalyos(4, i, szinem, 0, i)){lepesek.add(new Move(4, i, 0, i));}
-				
+			if(table.legal(4, i, color, 4, 0)){steps.add(new Move(4, i, 4, 0));}
+			if(table.legal(4, i, color, 4, 4)){steps.add(new Move(4, i, 4, 4));}
+			if(table.legal(4, i, color, 0, i)){steps.add(new Move(4, i, 0, i));}
 			//ha az elso oszlopbol valasztok
-			if(tabla.szabalyos(i, 0, szinem, 4, 0)){lepesek.add(new Move(i, 0, 4, 0));}
-			if(tabla.szabalyos(i, 0, szinem, 0, 0)){lepesek.add(new Move(i, 0, 0, 0));}
-			if(tabla.szabalyos(i, 0, szinem, i, 4)){lepesek.add(new Move(i, 0, i, 4));}
+			if(table.legal(i, 0, color, 4, 0)){steps.add(new Move(i, 0, 4, 0));}
+			if(table.legal(i, 0, color, 0, 0)){steps.add(new Move(i, 0, 0, 0));}
+			if(table.legal(i, 0, color, i, 4)){steps.add(new Move(i, 0, i, 4));}
 			//ha az utolso oszlopbol valasztok
-			if(tabla.szabalyos(i, 4, szinem, 0, 4)){lepesek.add(new Move(i, 4, 0, 4));}
-			if(tabla.szabalyos(i, 4, szinem, 4, 4)){lepesek.add(new Move(i, 4, 4, 4));}
-			if(tabla.szabalyos(i, 4, szinem, i, 0)){lepesek.add(new Move(i, 4, i, 0));}
+			if(table.legal(i, 4, color, 0, 4)){steps.add(new Move(i, 4, 0, 4));}
+			if(table.legal(i, 4, color, 4, 4)){steps.add(new Move(i, 4, 4, 4));}
+			if(table.legal(i, 4, color, i, 0)){steps.add(new Move(i, 4, i, 0));}
 		}
-		Collections.shuffle(lepesek); 		//Lepesek osszekeverese
-		Move m=lepesek.remove(0);
+		Collections.shuffle(steps); //steps mix
+		Move m=steps.remove(0);
 		return m;
-		*/
-	  return null;
 	}
-
+	
 	@Override
-	public int getSzin() {
-		return szinem;
+	public int getColor() {
+		return color;
 	}
 	
 	public String toString(){
 		return "RandomPlayer";
 	}
-
+	
 }
