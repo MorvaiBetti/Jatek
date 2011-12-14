d91b5e94a24084ce7a2568cb39448128e16b6743
Morvai-Szabó Beáta <morvaibetti@gmail.com>
2011-12-14 14:45:14 +0100

    Probalom megvalositani a min-max fat.

--- src/Moho.java
+++ src/Moho.java
@@ -2,29 +2,67 @@ public class Moho extends Tree{
 	
 	public Move nextMove(Move prevStep, long oTime) {
 		Pair p=new Pair(color, table);
-		tree = new TreeStructure(p, 1);
-		
-		
-		return null;
+		tree = new TreeStructure(p, 3, null, getColor());
+	//	max();
+		return max();
 	}
 	
 	public Move max(){
-		int i;
+		Node n, c = null;
+		n=tree.root;
+	
 		maxValue=-1000;
-		
-		for(i=tree.root.children.size(); i>=0; i--){
-		
-		}
-		
-		/*for(int i=0; i<25*5; i++){
-			if(values[i]>maxValue){
-				step=steps[i];
-				maxValue=values[i];
+		for(int i=0; i<n.children.size(); i++){
+			c=n.children.remove(i);
+			if(c.getValue()>maxValue){
+				step=c.getStep();
+				maxValue=c.getValue();
 			}
 		}
-		System.out.println("max: "+maxValue+" lepes: "+step);*/
+		step=undo(c);
+		System.out.println("max: "+maxValue+" lepes: "+step);
 		return step;
 	}
 	
+	public Move undo(Node n){
+		while(n.getIndex()!=1){
+			n=n.parent;
+		}
+		return n.getStep();
+	}
+
+}
+public class Moho extends Tree{
+	
+	public Move nextMove(Move prevStep, long oTime) {
+		Pair p=new Pair(color, table);
+		tree = new TreeStructure(p, 3, null, getColor());
+	//	max();
+		return max();
+	}
+	
+	public Move max(){
+		Node n, c = null;
+		n=tree.root;
+	
+		maxValue=-1000;
+		for(int i=0; i<n.children.size(); i++){
+			c=n.children.remove(i);
+			if(c.getValue()>maxValue){
+				step=c.getStep();
+				maxValue=c.getValue();
+			}
+		}
+		step=undo(c);
+		System.out.println("max: "+maxValue+" lepes: "+step);
+		return step;
+	}
+	
+	public Move undo(Node n){
+		while(n.getIndex()!=1){
+			n=n.parent;
+		}
+		return n.getStep();
+	}
 
 }
