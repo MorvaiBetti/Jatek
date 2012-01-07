f72b7b71704494e3343e71c1ba3c6f58fe285dba
Morvai-Szabó Beáta <morvaibetti@gmail.com>
2012-01-07 22:24:14 +0100

    Egy melysegig vizsgalja a jatekfat, alkalmazza a min-max algoritmust.

--- src/Moho.java
+++ src/Moho.java
@@ -1,37 +1,11 @@
 public class Moho extends SimplePlayer{
 	public Pair pair;
 	public TreeStructure tree;
+	public int depth=1;
 	
 	public Move nextMove(Move prevStep, long oTime) {
 		pair=new Pair(color, table);
-		tree = new TreeStructure(pair, 1, null, getColor());
-		return max();
-	}
-	
-	public Move max(){
-		Node c = null;
-		int maxValue=264*(-3);
-		for(int i=0; i<tree.newRoots.size(); i++){
-			c=tree.newRoots.remove(i);
-			if(c.getValue()>maxValue){
-				step=c.getStep();
-				maxValue=c.getValue();
-			}
-			if(c.isLeaf()){
-				step=undo(c);
-				return step;
-			}
-		}
-		step=undo(c);
-		tree=null;
-		return step;
-	}
-	
-	public Move undo(Node n){
-		while(n.index!=1){
-			System.out.println(n);
-			n=n.parent;
-		}
-		return n.getStep();
+		tree = new TreeStructure(pair, depth, null, getColor());
+		return tree.maxNode.step;
 	}
 }
